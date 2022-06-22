7dc4d7c798fb2d060fa79c4aaf24e8e06a59ade9
bahare <baharedadrasi@gmail.com>
2022-06-23 00:22:53 +0200

    route pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import pages
+import Home from './pages/Home';
+import About from './pages/About';
+import SingleCocktail from './pages/SingleCocktail';
+import Error from './pages/Error';
+import SharedLayout from './pages/SharedLayout';
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="cocktails/:id" element={<SingleCocktail />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/SingleCocktail.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SingleCocktail = () => {
+  return <h2>single cocktail</h2>;
+};
+
+export default SingleCocktail;
