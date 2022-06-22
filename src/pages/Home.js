baf204b4ea816e1e74222c0e23003906f08057c7
bahare <baharedadrasi@gmail.com>
2022-06-23 00:50:14 +0200

    add global style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <h2>home</h2>;
+  return (
+    <>
+      <h2>home</h2>
+      <button className="btn btn-primary">click me</button>
+    </>
+  );
 };
 
 export default Home;
